8d4c014f1c5adb0573d94eb70b892de42e89d8e9
Lenny Vongphouthone <91887834+lennyvong@users.noreply.github.com>
2022-04-11 17:27:54 +0200

    Delete task02.py

--- task02.py
+++
@@ -1,5 +0,0 @@
-def littleSpeech(firstWord: str, secondWord: str) -> str:
-    a = firstWord + secondWord
-    return a
-
-assert(littleSpeech("Hello", " World !") == "Hello World !"), "I guess he isn't alive."
